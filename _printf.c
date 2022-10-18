e11fe01efe1daf8d1e28f57822d5edb6f3a0b85a
Florence Coker Damilola <damselpiccolo18@gmail.com>
2022-10-18 21:04:18 +0100

    team project done

--- _printf.c
+++ _printf.c
@@ -53,10 +53,10 @@ int _printf(const char *format, ...)
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
+ *  * print_buffer - Prints the contents of the buffer if it exist
+ *   * @buffer: Array of chars
+ *    * @buff_ind: Index at which to add next char, represents the length.
+ *     */
 void print_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
